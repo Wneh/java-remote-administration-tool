d65dd921ac954cbdc7ee12ee1b0b3b63aa5f56f5
carle293 <carle293@gmail.com>
2011-04-28 08:08:51 +0200

    back to normal

--- src/master/CommandSender.java
+++ src/master/CommandSender.java
@@ -9,8 +9,6 @@ public class CommandSender{
 	private Socket s;
 	private PrintWriter out;
 	
-	//pwned
-	
 	public CommandSender(String host, int port){		
 		try {
 			s = new Socket(InetAddress.getByName(host),port);
