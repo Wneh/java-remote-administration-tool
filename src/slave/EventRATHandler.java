f7f5ed2060858db4ef298f8068b4eccf34e5ba3f
carle293 <carle293@localhost>
2012-03-04 17:40:54 +0100

    Made changes with the network class and the way to handle events Made an new class(EventRATHandler) for that purpose

---
+++ src/slave/EventRATHandler.java
@@ -0,0 +1,69 @@
+package slave;
+
+import java.awt.AWTException;
+
+import netPack.*;
+
+/**
+ * Handles incoming package of the type from EvenRAT
+ * 
+ * @author Carl
+ *
+ */
+
+public class EventRATHandler {
+	
+	private RobotSteer rs;
+	
+	public EventRATHandler(){
+		//Setup the robot class
+		rs = new RobotSteer();
+	}
+	/**
+	 * Takes in an event and execute the correct action for it
+	 * @param eventIn Someone that we want the execute
+	 */
+	public void handleEvent(EventRAT eventIn){
+		if(eventIn instanceof KeyEventRAT){
+			//We got an keyevent
+			executeKey((KeyEventRAT)eventIn);
+		}
+		else if(eventIn instanceof MouseEventRAT){
+			//We got and mouseevent
+			executeMouse((MouseEventRAT)eventIn);
+		}
+		else{
+			//We have no idea what we got! Print it out 
+			System.out.println("Recieved an unknown package type");
+		}
+	}
+	/**
+	 * Private class the handle mouse events 
+	 * @param mer And object of MouseEventRAT class
+	 */
+	private void executeMouse(MouseEventRAT mer){
+		//Check if the mouse was clicked
+		if(mer.isMouseClicked()){
+			//Left click
+			if(mer.getMc() == 1){
+				rs.clickLeft();
+			}
+			//Right click
+			else if(mer.getMc() == 3){
+				rs.clickRight();
+			}	
+		}
+		//Check if the mouse were moved
+		if(mer.isMouseClicked()){
+			rs.moveMouse(mer.getX(),mer.getY());
+		}
+	}
+	/**
+	 * Private class that executes a key 
+	 * @param ker An object of KeyEventRAT class
+	 */
+	private void executeKey(KeyEventRAT ker){
+		rs.typeKey(ker.getKeyCode());
+	}
+
+}
