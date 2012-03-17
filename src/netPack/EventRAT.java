e3d4dd72d11efca708734f65737cd6abfcd45e85
carle293 <carle293@localhost>
2012-03-17 12:36:14 +0100

    Added Serializable to the EventRAT class

--- src/netPack/EventRAT.java
+++ src/netPack/EventRAT.java
@@ -1,11 +1,15 @@
 package netPack;
 
+import java.io.Serializable;
+
 /**
  * Class that will be extended by the other
  * @author Carl
  *
  */
-public class EventRAT {
+public class EventRAT implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	/**
 	 * Does nothing at the moment
