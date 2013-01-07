44e601235a083b2d13b77561c9935074a99cf5b9
Carl <Carl@localhost>
2013-01-08 00:49:37 +0100

    Added methods for sending control commands over the socket with the new protocol

--- src/master/CommandThread.java
+++ src/master/CommandThread.java
@@ -1,8 +1,10 @@
 package master;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import netPack.EventRAT;
@@ -15,12 +17,18 @@ public class CommandThread extends Thread{
 	private ObjectOutputStream oout;
 	private GUIMASTER parent;
 	
+	private DataOutputStream dos;
+	private OutputStream out;
+	
 	public CommandThread(Socket s,GUIMASTER parent){
 		this.parent = parent;
 		this.s = s;
 		
 		//Try setting up the streams
 		try {
+			out = s.getOutputStream(); 
+			dos = new DataOutputStream(out);
+			
 			oout = new ObjectOutputStream(s.getOutputStream());			
 			oin = new ObjectInputStream(s.getInputStream());
 		} catch (IOException e) {
@@ -43,6 +51,61 @@ public class CommandThread extends Thread{
 		}
 		
 	}
+	/**
+	 * Sends keyEvent to the slave to execute
+	 * 
+	 * @param keyCode The key code for the key that shall used
+	 * @param keyState The state for the key(up or down)
+	 */
+	public void sendKeyEvent(int keyCode,byte keyState){
+		try {
+			//Start by sending what type of package see will send(Packet ID = 3 for keyEvent)
+			dos.writeByte(3);
+			//Continue sending the values
+			dos.writeInt(keyCode);
+			dos.writeByte(keyState);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("[Error] - something went from trying to send keyEvent");
+		}
+	}
+	/**
+	 * Sends a mousemove request to the slave
+	 * 
+	 * @param x The X coordinate of the mouse
+	 * @param y The Y coordinate of the mouse
+	 */
+	public void sendMouseMove(int x, int y){
+		try {
+			//Start by sending what type of package see will send(Packet ID = 1 for mouseMove)
+			dos.writeByte(1);
+			//Continue sending the values
+			dos.writeInt(x);
+			dos.writeInt(y);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("[Error] - something went from trying to send mouse move");
+		}
+	}
+	public void sendMouseClick(byte mouseKeyCode,byte mouseKeyState){
+		try {
+			//Start by sending what type of package see will send(Packet ID = 2 for mouse key click)
+			dos.writeByte(2);
+			//Continue sending the values
+			dos.writeByte(mouseKeyCode);
+			dos.writeByte(mouseKeyState);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
 	/**
 	 * Send a object of KeyEventRAT/MouseEventRAT to the slave to execute
 	 * @param toSend Instance of KeyEventRAT/MouseEventRAT
