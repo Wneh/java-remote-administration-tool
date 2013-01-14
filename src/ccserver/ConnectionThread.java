948fc3813ae50361b8f1e66f5ff21b4990e5fb73
carle293 <carle293@localhost>
2013-01-14 23:35:24 +0100

    More functionality added Client can connect and perform the handshake with the server

--- src/ccserver/ConnectionThread.java
+++ src/ccserver/ConnectionThread.java
@@ -1,5 +1,185 @@
 package ccserver;
 
-public class ConnectionThread {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+
+public class ConnectionThread extends Thread {
+	
+	private Socket s;
+	private int id;
+	private Server parent;
+	
+	private boolean running;
+	private boolean master;
+	private boolean inUse;
+	private String computerName;
+	
+	private OutputStream out;
+	private DataOutputStream dos;
+	private InputStream in;
+	private DataInputStream dis;
+	
+	public ConnectionThread(Socket s,int id, Server parent){
+		this.s = s;
+		this.id = id;
+		this.parent = parent;
+		
+		//Least privilege rule
+		this.running = false;
+		this.master = false;
+		this.inUse = false;
+		
+		//Create all the streams from the socket.
+		try {
+			out = s.getOutputStream();
+			dos = new DataOutputStream(out);
+			
+			in = s.getInputStream();
+			dis = new DataInputStream(in);
+		} catch (IOException e) {
+			parent.log.printlnErr("Something went wrong trying to setup input/output streams");
+			e.printStackTrace();
+		}	
+	}
+	
+	public void run(){
+		running = true;
+		byte packageID;
+		
+		try {
+			
+			running = doHandShake();
+			
+			parent.log.printlnOut(computerName + " connected to the server");
+			
+			while(running && ((packageID = dis.readByte()) != -1)){
+				
+				switch(packageID){
+					//Heartbeat
+					case 2:
+						// TODO Heartbeat functionality
+						break;
+					//Ask a slave to connect to a master	
+					case 3:
+						//Get the slaves ID
+						
+						//Get the masters ip
+						
+						//Get the masters port
+						break;
+					//Get the list of all slaves connected
+					case 4:
+						
+						break;
+				}
+				
+			}
+		} catch (IOException e) {
+			parent.log.printlnErr("IO exception from thread #"+id);
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * This will ask this thread´s client to connect to the IP
+	 * This method is called from another ConnectionThread that is a master
+	 * 
+	 * @param ip IP-address to the master
+	 * @param port Port that master is listening on
+	 * @return True if the thread is not busy
+	 */
+	public boolean connectSlaveToMaster(String ip,int port){
+		//Check so that the thread is still alive and it not a master and that it not allready in use
+		if(running && !master && !inUse){
+			//Set that this thread is in use
+			inUse = true;			
+			//Now send a request to the slave to connect to the master
+				
+			try {
+				//PackageID = 10
+				dos.writeByte(10);
+				//Send the ip
+				writeString(ip);
+				//Send the port number
+				dos.writeInt(port);
+			} catch (IOException e) {
+				parent.log.printlnErr("Something went wrong trying to setup input/output streams");
+				e.printStackTrace();
+			}
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/**
+	 * This handshake is very similar to the TCP 3-way handshake
+	 * A more detailed description about it will probably be added
+	 * to the wiki
+	 * 
+	 * @return True or False depending how the handshake went
+	 */
+	private boolean doHandShake(){
+		byte packageID;
+		byte content;
+	
+		//First we should get a package from the client saying like "Hello there"
+		try {
+			packageID = dis.readByte();
+			content = dis.readByte();
+			//PackageID shall have value 1
+			if(packageID == 1 && content == 1){
+				//Everything okey so far so answer the request with packageID = and content =2
+				//Send packageID
+				dos.writeByte(1);
+				//Send content
+				dos.writeByte(2);
+				
+				//Now we should get a response with packageID = 1 and a String with the name of the computer
+				packageID = dis.readByte();
+				computerName = readString();
+				
+				//PackageID shall have value 1 and the compuerName shall not be null anymore
+				if(packageID == 1 && computerName != null){
+					//Everything okey so return true
+					return true;
+				}
+			}
+		} catch (IOException e) {
+			parent.log.printlnErr("I/O Exception while performing handshake");
+			e.printStackTrace();
+		}
+		return false;
+	}
+	private void writeString(String toWrite){
+		try {
+			byte[] data = toWrite.getBytes("UTF-8");
+			dos.writeInt(data.length);
+			dos.write(data);
+		} catch (UnsupportedEncodingException e) {
+			parent.log.printlnErr("Wrong enconding on String in writeString()");
+			e.printStackTrace();
+		} catch (IOException e) {
+			parent.log.printlnErr("IO exception trying to send string over DataOutputStream");
+			e.printStackTrace();
+		}
+	}
+	private String readString(){
+		try {
+			int length = dis.readInt();
+			byte data[] = new byte[length];
+			dis.readFully(data);
+			return new String(data,"UTF-8");
+		} catch (IOException e) {
+			parent.log.printlnErr("IO exception trying to read string from DataInputStream");
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
--- src/ccserver/Server.java
+++ src/ccserver/Server.java
@@ -1,5 +1,82 @@
 package ccserver;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
 
+public class Server extends Thread {
+	
+	public Log log;
+	private int port;
+	private ServerSocket ss;
+	private boolean running;
+	
+	int idCounter;
+	
+	private ArrayList<ConnectionThread> connections;
+	
+	public Server(){
+		this.port = 1999;
+	}
+	public Server(int port){
+		this.port = port;
+	}
+	
+	public void run(){
+		//Star with setting everything up
+		initializeServer();
+		
+		//Keep waiting for connection until something say not to by setting running to false
+		log.printlnOut("Waiting for connection...");
+		while(running){
+			try {
+				ConnectionThread ct = new ConnectionThread(ss.accept(),idCounter,this);
+				idCounter++;
+				connections.add(ct);
+				ct.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//Before the thread dies we need to close everything
+		stopServer();
+		
+		//RIP this.Thread		
+	}
+	
+	/**
+	 * Use this make everything ready for receiving connections
+	 */
+	public void initializeServer(){
+		//Create a new logger to use
+		log = new Log();
+		idCounter = 1;
+		connections = new ArrayList<ConnectionThread>();
+		
+		log.printlnOut("Starting server");
+		
+		//Try to make a server socket
+		try {
+			ss = new ServerSocket(port);
+			running = true;
+		} catch (IOException e) {
+			log.printlnErr("Could not start ServerSocket. Is the port allready in use?");
+			running = false;
+		}
+		
+		log.printlnOut("Server started!");
+	}
+	
+	/**
+	 * Use this to close everything properly
+	 */
+	public void stopServer(){
+		// TODO 
+	}
+	
+	public static void main(String[] args) {
+		new Thread(new Server()).start();
+	}
 }
