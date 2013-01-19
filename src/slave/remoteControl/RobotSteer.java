4f7fa7f34c89578e64fd774b16334c29a5ed9698
carle293 <carle293@localhost>
2013-01-19 21:59:17 +0100

    Created new sub package to get better structure

---
+++ src/slave/remoteControl/RobotSteer.java
@@ -0,0 +1,68 @@
+package slave.remoteControl;
+
+
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.InputEvent;
+/**
+ * Help class that use the java Robot class
+ * @author Carl
+ */
+public class RobotSteer{
+	
+	private Robot rBot;
+	
+	/**
+	 * Creates a new Robot
+	 */
+	public RobotSteer(){
+		try{
+			rBot = new Robot();
+		}
+		 catch (AWTException e) { 
+			 System.err.println("[ERROR] - Could not create the robot object");
+			 e.printStackTrace(); 
+		 } 
+	}
+	public void clickMouse(byte mouseKeyCode, byte mouseKeyCodeState){
+		//Key is pressed
+		if(mouseKeyCodeState == 1){
+			if(mouseKeyCode == 1){
+				rBot.mousePress(InputEvent.BUTTON1_MASK);
+			}
+			else if(mouseKeyCode == 3){
+				rBot.mousePress(InputEvent.BUTTON3_MASK);
+			}
+		}
+		//Key is released
+		else{
+			if(mouseKeyCode == 1){
+				rBot.mouseRelease(InputEvent.BUTTON1_MASK);
+			}
+			else if(mouseKeyCode == 3){
+				rBot.mouseRelease(InputEvent.BUTTON3_MASK);
+			}
+		}
+	}
+	
+	public void clickKey(int keyCode,byte keyState){
+		//Press the down a key
+		if(keyState == 1){
+			rBot.keyPress(keyCode);
+		}
+		//Release the key
+		else{
+			rBot.keyRelease(keyCode);
+		}
+	}
+	
+	/**
+	 * Moves the mouse the x and y.
+	 * @param x
+	 * @param y
+	 */
+	public void moveMouse(int x, int y){
+		System.out.println("[INFO] - Moving mouse to X: "+x+" Y: "+y);
+		rBot.mouseMove(x, y);
+	}
+}
